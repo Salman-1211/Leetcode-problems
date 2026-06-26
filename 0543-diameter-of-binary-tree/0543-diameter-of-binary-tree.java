e67dfa8dd6d32ab47a9a40e28f8fc58dce8dfbab
Salman Mulani <76278177+Salman-1211@users.noreply.github.com>
2026-06-26 12:11:41 +0530

    Time: 1 ms (44.51%), Space: 46.8 MB (92.92%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -15,40 +15,48 @@
  */
 class Solution {
     
-    // Global variable to
-    // store the diameter
-    int diameter = 0;
+    // Function to find the
+    // diameter of a binary tree
+    public int diameterOfBinaryTree(TreeNode root) {
+        
+        // Initialize the variable to
+        // store the diameter of the tree
+        int[] diameter = new int[1];
+        diameter[0] = 0;
+        
+        // Call the height function to traverse
+        // the tree and calculate diameter
+        height(root, diameter);
+        
+        // Return the calculated diameter
+        return diameter[0];
+    }
 
-    // Function to calculate
-    // the height of a subtree
-        int calculateHeight(TreeNode node) {
+    // Function to calculate the height of
+    // the tree and update the diameter
+    private int height(TreeNode node, int[] diameter) {
+        
+        // Base case: If the node is null,
+        // return 0 indicating the
+        // height of an empty tree
         if (node == null) {
             return 0;
         }
 
         // Recursively calculate the
         // height of left and right subtrees
-        int leftHeight = calculateHeight(node.left);
-        int rightHeight = calculateHeight(node.right);
+        int[] lh = new int[1];
+        int[] rh = new int[1];
+        lh[0] = height(node.left, diameter);
+        rh[0] = height(node.right, diameter);
 
-        // Calculate the diameter at the current
-        // node and update the global variable
-        diameter = Math.max(diameter, leftHeight + rightHeight);
+        // Update the diameter with the maximum
+        // of current diameter or sum of
+        // left and right heights
+        diameter[0] = Math.max(diameter[0], lh[0] + rh[0]);
 
-        // Return the height
-        // of the current subtree
-        return 1 + Math.max(leftHeight, rightHeight);
-    }
-
-    // Function to find the
-    // diameter of a binary tree
-    int diameterOfBinaryTree(TreeNode root) {
-        // Start the recursive
-        // traversal from the root
-        calculateHeight(root);
-
-        // Return the maximum diameter
-        // found during traversal
-        return diameter;
+        // Return the height of
+        // the current node's subtree
+        return 1 + Math.max(lh[0], rh[0]);
     }
 }
