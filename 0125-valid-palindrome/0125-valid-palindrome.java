0a2f1ea4b5927c3d5f57eef46d8a2fbbbdf6949e
Salman Mulani <76278177+Salman-1211@users.noreply.github.com>
2026-08-04 23:18:18 +0530

    Time: 4 ms (46.24%), Space: 45.4 MB (35.7%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,26 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int l = 0, r=s.length()-1;
+        StringBuilder newStr = new StringBuilder();
 
-        while(l<r){
-            while(l<r && !alphaNum(s.charAt(l))){
-                l++;
+        for(char c : s.toCharArray()){
+            if(Character.isLetterOrDigit(c)){
+                newStr.append(Character.toLowerCase(c));
             }
-            while(r>l && !alphaNum(s.charAt(r))){
-                r--;
-            }
-
-            if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))){
-                return false;
-            }
-            l++;
-            r--;
         }
-        return true;
-        }
-    public boolean alphaNum(char c){
-        return (c >= 'A' && c <='Z' ||
-                c >= 'a' && c <= 'z'||
-                c >='0' && c <= '9');
+        return newStr.toString().equals(newStr.reverse().toString());
     } 
 }
