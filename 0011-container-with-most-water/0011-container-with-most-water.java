fd8913c9ba1817191e75206c0079055838da82a7
Salman Mulani <76278177+Salman-1211@users.noreply.github.com>
2026-09-13 00:15:13 +0530

    Time: 5 ms (82.97%), Space: 77.5 MB (12.77%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,7 +1,6 @@
 class Solution {
     public int maxArea(int[] height) {
-
-        int left = 0;
+       int left = 0;
         int right = height.length - 1;
 
         int maxArea = 0;
@@ -28,6 +27,7 @@ class Solution {
         }
 
         return maxArea;
+ 
  
     }
 }
